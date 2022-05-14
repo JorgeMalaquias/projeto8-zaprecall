b60b1200a56e400dd24901830788ba42c6f42f69
Jorge <fulano@email.com>
2022-05-14 03:04:44 -0300

    tela1 finalizada layout

---
+++ src/components/screen1.js
@@ -0,0 +1,11 @@
+import logo from "../assets/img/logo.png"
+
+export default function Screen1(){
+    return (
+        <div className="screen1">
+            <img src={logo}/>
+            <h1>ZapRecall</h1>
+            <button>Inicir Recall!</button>
+        </div>
+    );
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import App from "./components/app";
+import ReactDom from "react-dom";
+import "./assets/css/style.css";
+
+ReactDom.render(<App/>,document.querySelector(".root"));
